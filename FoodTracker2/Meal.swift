fe2d0c7ab7fb1f31c1603a7886623d960d7b230f
magicpie2007 <magicpie2007@gmail.com>
2016-11-13 17:59:51 +0900

    Finish "Define Your Data Model"

---
+++ FoodTracker2/Meal.swift
@@ -0,0 +1,29 @@
+//
+//  Meal.swift
+//  FoodTracker2
+//
+//  Created by 小路宏幸 on 2016/11/13.
+//  Copyright © 2016年 hiroyuki.shoji. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
